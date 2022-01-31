b314447c5a0c3749947232b31a0ff6a7e874d9ec
rhit-snownp <snownp@rose-hulman.edu>
2022-01-30 22:59:45 -0500

    Setup all the possible options

--- NYU_Dataset_Test.m
+++ NYU_Dataset_Test.m
@@ -10,23 +10,24 @@ load('NYU Dataset\nyu_depth_v2_labeled.mat')
 
 %%
 % trainX = images(:,:,:,1:round(1449/2));
-% trainY = depths(:,:,1:round(1449/2));
+trainY = depths(:,:,1:round(1449/2));
 % testX = images(:,:,:,round(1449/2)+1:end);
-% testY = depths(:,:,round(1449/2)+1:end);
-
-trainX = imageDatastore("NYU Dataset/Training Data/Input/");
-trainY = imageDatastore("NYU Dataset/Training Data/Output/");
-testX = imageDatastore("NYU Dataset/Testing Data/Input/");
-testY = imageDatastore("NYU Dataset/Testing Data/Output/");
-
+testY = depths(:,:,round(1449/2)+1:end);
 newTrainY = repmat(reshape(trainY, size(trainY,1), size(trainY,2), 1, size(trainY,3)), 1, 1, 3, 1);
 newTestY = repmat(reshape(testY, size(testY,1), size(testY,2), 1, size(testY,3)), 1, 1, 3, 1);
+
+trainX = imageDatastore("NYU Dataset/Training Data/Input/",'LabelSource','foldernames');
+% trainY = imageDatastore("NYU Dataset/Training Data/Output/",'LabelSource','foldernames');
+testX = imageDatastore("NYU Dataset/Testing Data/Input/",'LabelSource','foldernames');
+% testY = imageDatastore("NYU Dataset/Testing Data/Output/",'LabelSource','foldernames');
+
+
 
 
 %Rescale input data to 304×228x1
 imageSize = uint8([304 228 1]);
-augmentedTrainImages = augmentedImageDatastore(imageSize,trainX,'ColorPreprocessing','rgb2gray');
-augmentedTestImages = augmentedImageDatastore(imageSize,testX,'ColorPreprocessing','rgb2gray');
+augmentedTrainImages = augmentedImageDatastore(imageSize,trainX,trainY,'ColorPreprocessing','rgb2gray');
+augmentedTestImages = augmentedImageDatastore(imageSize,testX,testY,'ColorPreprocessing','rgb2gray');
 %Load in the defined Pipeline 1 Network Architecture
 
 [layers] = pipeline1();
