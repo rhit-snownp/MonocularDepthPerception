a62f4c2b20aae91ee049dc615b1d1dff3d355159
rhit-snownp <snownp@rose-hulman.edu>
2022-01-30 22:52:26 -0500

    Attempted to train network

---
+++ NYU Dataset/ExtractNYUDataset.m
@@ -0,0 +1,27 @@
+%Extract NYU Dataset into Pairs
+clc;
+clear all
+close all;
+
+load("nyu_depth_v2_labeled.mat");
+
+%% 
+
+tic
+
+for currentImageIndex = 1:length(scenes)
+
+%Extract a depth image, and the file name and save it
+currentDepthImage = depths(:,:,currentImageIndex);
+depthImageFilename = char(rawDepthFilenames(currentImageIndex));
+
+%Extract an image, and the file name and save it
+currentImage = images(:,:,:,currentImageIndex);
+imageFilename = char(rawRgbFilenames(currentImageIndex));
+
+imwrite(currentDepthImage,sprintf("Depth %03d.png",currentImageIndex),'png');
+imwrite(currentImage,sprintf("Image %03d.png",currentImageIndex),'png');
+
+end
+
+toc
--- NYU_Dataset_Test.m
+++ NYU_Dataset_Test.m
@@ -6,13 +6,41 @@ close all
 
 
 %Load in the NYU Depth Dataset
-relativePath = "nyu_depth_v2_labeled.mat";
-[inputDataDepths,inputDataImages] = ReadNYUDepthToDatastore(relativePath);
+load('NYU Dataset\nyu_depth_v2_labeled.mat')
+
+%%
+% trainX = images(:,:,:,1:round(1449/2));
+% trainY = depths(:,:,1:round(1449/2));
+% testX = images(:,:,:,round(1449/2)+1:end);
+% testY = depths(:,:,round(1449/2)+1:end);
+
+trainX = imageDatastore("NYU Dataset/Training Data/Input/");
+trainY = imageDatastore("NYU Dataset/Training Data/Output/");
+testX = imageDatastore("NYU Dataset/Testing Data/Input/");
+testY = imageDatastore("NYU Dataset/Testing Data/Output/");
+
+newTrainY = repmat(reshape(trainY, size(trainY,1), size(trainY,2), 1, size(trainY,3)), 1, 1, 3, 1);
+newTestY = repmat(reshape(testY, size(testY,1), size(testY,2), 1, size(testY,3)), 1, 1, 3, 1);
 
 
-
+%Rescale input data to 304×228x1
+imageSize = uint8([304 228 1]);
+augmentedTrainImages = augmentedImageDatastore(imageSize,trainX,'ColorPreprocessing','rgb2gray');
+augmentedTestImages = augmentedImageDatastore(imageSize,testX,'ColorPreprocessing','rgb2gray');
 %Load in the defined Pipeline 1 Network Architecture
 
- [layers] = pipeline1();
- analyzeNetwork(layers)
+[layers] = pipeline1();
 
+
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',128, ...
+    'MaxEpochs',30, ...
+    'InitialLearnRate',1e-3, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropFactor',0.1, ...
+    'LearnRateDropPeriod',20, ...
+    'Shuffle','every-epoch', ...
+    'Plots','training-progress', ...
+    'Verbose',false);
+%%
+net = trainNetwork(trainX,newTrainY,layers,options);
