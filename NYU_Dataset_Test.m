9263ad684e6619a42bc6027e69195d85cbc19684
rhit-snownp <snownp@rose-hulman.edu>
2022-01-31 23:26:14 -0500

    Slight Change to get things actually trainable - now a layer problem

--- NYU_Dataset_Test.m
+++ NYU_Dataset_Test.m
@@ -9,26 +9,37 @@ close all
 load('NYU Dataset\nyu_depth_v2_labeled.mat')
 
 %%
-% trainX = images(:,:,:,1:round(1449/2));
-trainY = depths(:,:,1:round(1449/2));
-% testX = images(:,:,:,round(1449/2)+1:end);
-testY = depths(:,:,round(1449/2)+1:end);
-newTrainY = repmat(reshape(trainY, size(trainY,1), size(trainY,2), 1, size(trainY,3)), 1, 1, 3, 1);
-newTestY = repmat(reshape(testY, size(testY,1), size(testY,2), 1, size(testY,3)), 1, 1, 3, 1);
+% % trainX = images(:,:,:,1:round(1449/2));
+% trainY = depths(:,:,1:round(1449/2));
+% % testX = images(:,:,:,round(1449/2)+1:end);
+% testY = depths(:,:,round(1449/2)+1:end);
+% newTrainY = repmat(reshape(trainY, size(trainY,1), size(trainY,2), 1, size(trainY,3)), 1, 1, 3, 1);
+% newTestY = repmat(reshape(testY, size(testY,1), size(testY,2), 1, size(testY,3)), 1, 1, 3, 1);
 
 trainX = imageDatastore("NYU Dataset/Training Data/Input/",'LabelSource','foldernames');
-% trainY = imageDatastore("NYU Dataset/Training Data/Output/",'LabelSource','foldernames');
+trainY = imageDatastore("NYU Dataset/Training Data/Output/",'LabelSource','foldernames');
 testX = imageDatastore("NYU Dataset/Testing Data/Input/",'LabelSource','foldernames');
-% testY = imageDatastore("NYU Dataset/Testing Data/Output/",'LabelSource','foldernames');
+testY = imageDatastore("NYU Dataset/Testing Data/Output/",'LabelSource','foldernames');
 
 
 
 
-%Rescale input data to 304×228x1
-imageSize = uint8([304 228 1]);
-augmentedTrainImages = augmentedImageDatastore(imageSize,trainX,trainY,'ColorPreprocessing','rgb2gray');
-augmentedTestImages = augmentedImageDatastore(imageSize,testX,testY,'ColorPreprocessing','rgb2gray');
+%Rescale input data to 304x228x1
+% imageSize = uint8([304 228 1]);
+% augmentedTrainImages = augmentedImageDatastore(imageSize,trainX,trainY,'ColorPreprocessing','rgb2gray');
+% augmentedTestImages = augmentedImageDatastore(imageSize,testX,testY,'ColorPreprocessing','rgb2gray');
 %Load in the defined Pipeline 1 Network Architecture
+
+
+imageSize = uint8([304 228 1]);
+augmentedTrainX = augmentedImageDatastore(imageSize,trainX,'ColorPreprocessing','rgb2gray');
+augmentedTrainY = augmentedImageDatastore(imageSize,trainY);
+
+dsTrain = combine(augmentedTrainX,augmentedTrainY);
+
+
+
+analyze(layers)
 
 [layers] = pipeline1();
 
@@ -44,4 +55,4 @@ options = trainingOptions('sgdm', ...
     'Plots','training-progress', ...
     'Verbose',false);
 %%
-net = trainNetwork(trainX,newTrainY,layers,options);
+net = trainNetwork(dsTrain,layers,options);
