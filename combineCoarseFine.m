e05f934faa287daf1a56345c49d19ed8290d543f
rhit-snownp <snownp@rose-hulman.edu>
2022-02-10 10:25:44 -0500

    Slight Tweaks to Fine Network

--- combineCoarseFine.m
+++ combineCoarseFine.m
@@ -25,24 +25,64 @@ options = trainingOptions("adam", ...
     'Plots','training-progress');
 
 net = trainNetwork(trainCombined,lgraph,options);
-
-
-
-save("coarseNet3.mat",'net');
-
+save("Combined Network 1",'net');
+YPred = squeeze(predict(net,valCombined));
 
 %%
-% limit = 10;
-% for index = 1:limit
-% 
-%     inputImages = read(valCombined);
-%     figure;
-%     subplot(1,2,1);
-%     imshow(inputImages{1});
-%     title("Input Image");
-%     subplot(1,2,2);
-% 
-%     imagesc(inputImages{2});
-%     title("Depth Image");
-%     
-% end
+limit = 10;
+
+for index = 1:limit
+    inputImages = read(valCombined);
+    figure;
+    subplot(2,2,1);
+    imshow(inputImages{1});
+    title("Input Image");
+    subplot(2,2,3);
+    imagesc(inputImages{2});
+    title("Depth Image");
+    subplot(2,2,4);
+    imagesc(YPred(:,:,index));
+    title("Output");
+end
+
+
+
+
+%% Calculate threshold metrics
+load("Combined Network 1")
+testCombined = ReadTestData("images\val");
+out = exp(predict(net, testCombined));
+target_depth_cells = readall(testCombined.UnderlyingDatastores{2});
+test_data_length = length(target_depth_cells);
+target_depths = reshape(cat(3,target_depth_cells{:}),[76 57 1 test_data_length]);
+
+%%
+sigma_125 = calculate_threshold_metric(out, target_depths, 1.25)
+sigma_125_2 = calculate_threshold_metric(out, target_depths, 1.25^2)
+sigma_125_3 = calculate_threshold_metric(out, target_depths, 1.25^3)
+
+
+function [testCombined] = ReadTestData(relativePath)
+    inputDataImages = imageDatastore(relativePath,"ReadFcn", @loadImage,"IncludeSubfolders",true);
+%     augDataImages = augmentedImageDatastore([304, 228], inputDataImages);
+    inputDataDepths = imageDatastore(relativePath, 'ReadFcn',@loadDIODEZDepth,'FileExtensions','.npy',"IncludeSubfolders",true);
+%     augDataDepths = augmentedImageDatastore([76,57], inputDataDepths);
+
+    testCombined = combine(inputDataImages, inputDataDepths);
+
+    function data = loadDIODEZDepth(filename)
+        addpath npy-matlab\
+        data = readNPY(filename);
+        data = imresize(data,[76,57]);
+    end
+
+    function data = loadImage(filename)
+        im = imread(filename);
+        im = imgaussfilt(im,2);
+        data = imresize(im, [304 228]);
+    end
+end
+
+
+
+   
