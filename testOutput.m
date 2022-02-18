741463ad8491a8e1c58bdeb1b0e2d61b3c847000
Michael Hall <28988176+mich-431@users.noreply.github.com>
2022-02-17 20:51:04 -0500

    small delta calculation changes

--- testOutput.m
+++ testOutput.m
@@ -3,26 +3,41 @@ clc; close all; clear variables;
 load("combinedNet4.mat");
 testCombined = ReadTestData("images\test");
 
-%% Calculate threshold metric
+%% Pass test set through network
 out = exp(predict(net, testCombined));
-target_depth_cells = readall(testCombined.UnderlyingDatastores{2});
+target_depth_cells = readall(testCombined.UnderlyingDatastores{2}); %get the ground truth data
 test_data_length = length(target_depth_cells);
 target_depths = reshape(cat(3,target_depth_cells{:}),[76 57 test_data_length]);
 out = squeeze(out);
 predictions = zeros(size(out));
 
+
+%%
+u = 95; %upper precentile for scaling
+l = 100-u; %lower
+
 for i=1:size(out,3)
-    p95 = prctile(reshape(target_depths(:,:,i),1,[]),95);
-    p5 = prctile(reshape(target_depths(:,:,i),1,[]),5);
-    predictions(:,:,i) = rescale(out(:,:,i), p5, p95);
+    outDepth = out(:,:,i);
+    targetmax = prctile(reshape(target_depths(:,:,i),1,[]),u);
+    targetmin = prctile(reshape(target_depths(:,:,i),1,[]),l);
+    outputmax = prctile(reshape(outDepth,1,[]),u);
+    outputmin = prctile(reshape(outDepth,1,[]),l);
+    predictions(:,:,i) = targetmin + [(outDepth-outputmin)./(outputmax-outputmin)].*(targetmax-targetmin);
 end
 
 
 %% 
-sigma_125 = calculate_threshold_metric(out, target_depths, 1.25)
-sigma_125_2 = calculate_threshold_metric(out, target_depths, 1.25^2)
-sigma_125_3 = calculate_threshold_metric(out, target_depths, 1.25^3)
+% delta values before scaling
+disp("before scaling")
+delta_125 = calculate_threshold_metric(out, target_depths, 1.25)
+delta_125_2 = calculate_threshold_metric(out, target_depths, 1.25^2)
+delta_125_3 = calculate_threshold_metric(out, target_depths, 1.25^3)
 
+%delta values after scaling
+disp("after scaling")
+delta_125 = calculate_threshold_metric(predictions, target_depths, 1.25)
+delta_125_2 = calculate_threshold_metric(predictions, target_depths, 1.25^2)
+delta_125_3 = calculate_threshold_metric(predictions, target_depths, 1.25^3)
 %% Test individual images
 
 images = testCombined.read();
@@ -30,13 +45,13 @@ inputImg = images{1};
 groundTruth = images{2};
 
 out = exp(predict(net, inputImg));
-rescaledOutput = rescale(out, prctile(groundTruth(:),5),prctile(groundTruth(:),95));
-sigma = calculate_threshold_metric(out, groundTruth, 1.25)
-sigma2 = calculate_threshold_metric(rescaledOutput, groundTruth, 1.25)
-subplot(2,2,4); imagesc(rescaledOutput);colorbar;axis equal;
-subplot(2,2,3); imshow(inputImg);
-subplot(2,2,2); imagesc(out); title("output depthmap");colorbar;axis equal;
-subplot(2,2,1); imagesc(images{2}); title("ground truth");colorbar; axis equal;
+rescaledOutput = rescale(out, prctile(groundTruth(:),l),prctile(groundTruth(:),u));
+delta = calculate_threshold_metric(out, groundTruth, 1.25)
+delta2 = calculate_threshold_metric(rescaledOutput, groundTruth, 1.25)
+subplot(2,2,4); imagesc(rescaledOutput);title("Scaled Output Depth Map \delta < 1.25:",sprintf(" %1.3f",delta2));colorbar;axis equal;
+subplot(2,2,1); imshow(inputImg); title("Input Image");
+subplot(2,2,3); imagesc(out); title("Output Depth Map \delta < 1.25:",sprintf(" %1.3f",delta));colorbar;axis equal;
+subplot(2,2,2); imagesc(groundTruth); title("Depth Image");colorbar; axis equal;
 
 
 function [testCombined] = ReadTestData(relativePath)
